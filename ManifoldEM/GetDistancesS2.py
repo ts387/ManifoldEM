ts387/ManifoldEM

commit e09b8a5f80929446eeae1afb71abedbf652fbaf2

distances: remove more unused intermediate data

ManifoldEM/GetDistancesS2.py
@@ -158,18 +158,17 @@ class LocalInput:
 
 def get_distance_CTF_local(input_data: LocalInput, filter_params: FilterParams, img_file_name: str,
                            image_offsets: Tuple[NDArray[Shape["*"], Float64], NDArray[Shape["*"], Float64]],
-                           n_particles_tot: int, avg_only: bool, relion_data: bool):
+                           n_particles_tot: int, relion_data: bool):
     """
     Calculates squared Euclidian distances for snapshots in similar
     projection directions. Includes CTF correction of microscope.
     Version with conjugates, effectively double number of data points
 
     Input parameters
-    input_data see LocalInput
+    input_data     see LocalInput
     filter_params  Filter Gaussian width [pixel]
     image_offsets  Image origins (from star files, usually. aka "sh")
     img_file_name  Image file with all raw images
-    avg_only       Skip calculation of distances
 
     Uses the following microscope data from global params:
         Cs         Spherical aberration [mm]
@@ -178,7 +177,6 @@ def get_distance_CTF_local(input_data: LocalInput, filter_params: FilterParams, 
         nPix       lateral pixel count
         dPix       Pixel size [A]
     """
-    version = 'getDistanceCTF_local9, V 1.0'
     indices = input_data.indices
     quats = input_data.quats
     defocus = input_data.defocus
@@ -188,12 +186,8 @@ def get_distance_CTF_local(input_data: LocalInput, filter_params: FilterParams, 
     # auxiliary variables
     n_pix = p.nPix
 
-    # initialize arrays
-    psis = np.nan * np.ones((n_particles, 1))  # psi angles
     # different types of averages of aligned particles of the same view
     img_avg = np.zeros((n_pix, n_pix))  # simple average
-    img_avg_flip = np.zeros((n_pix, n_pix))  # average of phase-flipped particles
-    img_avg_intensity = np.zeros((n_pix, n_pix))
     img_all = np.zeros((n_particles, n_pix, n_pix))
 
     fourier_images = np.zeros((n_particles, n_pix, n_pix), dtype=np.complex128)  # each (i,:,:) is a Fourier image
@@ -223,28 +217,21 @@ def get_distance_CTF_local(input_data: LocalInput, filter_params: FilterParams, 
         msk2 = 1
 
     # read images with conjugates
-    img_labels = np.zeros(n_particles, dtype=int)
     for i_part in range(n_particles):
         if indices[i_part] < n_particles_tot / 2:  # first half data set; i.e., before augmentation
-            raw_particle_index = int(indices[i_part])
-            img_labels[i_part] = 1
+            raw_particle_index = indices[i_part]
         else:  # second half data set; i.e., the conjugates
-            raw_particle_index = int(indices[i_part] - n_particles_tot / 2)
-            img_labels[i_part] = -1
-            # matlab version: y[:,iS] = m.Data(ind(iS)).y
+            raw_particle_index = (indices[i_part] - n_particles_tot) // 2
         if not relion_data:  # spider data
             start = n_pix**2 * raw_particle_index * 4
-            img = np.memmap(img_file_name, dtype='float32', offset=start, mode='r', shape=(n_pix, n_pix))
-            # store each flatted image in y
-            img = img.T  # numpy mapping is diff from matlab's
+            img = np.memmap(img_file_name, dtype='float32', offset=start, mode='r', shape=(n_pix, n_pix)).T
         else:  # relion data
-            img = mrcfile.mmap(img_file_name, 'r')
-            img.is_image_stack()
-            img = img.data[raw_particle_index]
+            img = mrcfile.mmap(img_file_name, 'r').data[raw_particle_index]
             shi = (image_offsets[1][raw_particle_index] - 0.5, image_offsets[0][raw_particle_index] - 0.5)
             img = shift(img, shi, order=3, mode='wrap')
         if indices[i_part] >= n_particles_tot / 2:  # second half data set
             img = np.flipud(img)
+
         # normalizing
         backg = img * (1 - msk)
         std = backg.std()
@@ -256,8 +243,7 @@ def get_distance_CTF_local(input_data: LocalInput, filter_params: FilterParams, 
         # store each flatted image in y and filter
         img = img.flatten('F')
         img = img.reshape(-1, n_pix).transpose()
-        img = ifft2(fft2(img) * G).real
-        img = img.real.flatten('F')
+        img = ifft2(fft2(img) * G).real.flatten('F')
 
         # Get the psi angle
         psi = get_psi(quats[:, i_part], avg_orientation_vec)
@@ -265,7 +251,6 @@ def get_distance_CTF_local(input_data: LocalInput, filter_params: FilterParams, 
         # this happens only for a rotation of pi about an axis perpendicular to the projection direction
         if np.isnan(psi):
             psi = 0.
-        psis[i_part] = psi  # save image rotations
 
         # inplane align the images
         img = img.reshape(-1, n_pix).transpose() * msk  # convert to matlab convention prior to rotation
@@ -274,15 +259,11 @@ def get_distance_CTF_local(input_data: LocalInput, filter_params: FilterParams, 
 
         # CTF info
         ctf_i = ctemh_cryoFrank(Q / (2 * p.pix_size), p.Cs, defocus[i_part], p.EkV, p.gaussEnv, p.AmpContrast)
-
-        CTF[i_part, :, :] = ifftshift(ctf_i)  # tmp should be in matlab convention
+        CTF[i_part, :, :] = ifftshift(ctf_i)
 
         # Fourier transformed #April 2020, with vol mask msk2, used for distance calc D
         fourier_images[i_part, :, :] = fft2(img * msk2)
 
-        img_flip = ifft2(np.sign(CTF[i_part, :, :]) * fourier_images[i_part, :, :])  # phase-flipped
-        img_avg_flip = img_avg_flip + img_flip.real  # average of all phase-flipped images
-        img_avg_intensity += img_flip.real**2 / n_particles
         img_all[i_part, :, :] = img
 
     # use wiener filter
@@ -298,33 +279,23 @@ def get_distance_CTF_local(input_data: LocalInput, filter_params: FilterParams, 
     # plain and phase-flipped averages
     # April 2020, msk2 = 1 when there is no volume mask
     img_avg = img_avg * msk2 / n_particles
-    img_avg_flip = img_avg_flip.real * msk2 / n_particles
 
-    if not avg_only:
-        fourier_images = fourier_images.reshape(n_particles, n_pix**2)
-        CTF = CTF.reshape(n_particles, n_pix**2)
+    fourier_images = fourier_images.reshape(n_particles, n_pix**2)
+    CTF = CTF.reshape(n_particles, n_pix**2)
 
-        CTFfy = CTF.conj() * fourier_images
-        distances = np.dot((np.abs(CTF)**2), (np.abs(fourier_images)**2).T)
-        distances = distances + distances.T - 2 * np.real(np.dot(CTFfy, CTFfy.conj().transpose()))
+    CTFfy = CTF.conj() * fourier_images
+    distances = np.dot((np.abs(CTF)**2), (np.abs(fourier_images)**2).T)
+    distances = distances + distances.T - 2 * np.real(np.dot(CTFfy, CTFfy.conj().transpose()))
 
     myio.fout1(out_file,
                D=distances,
                ind=indices,
                q=quats,
-               df=defocus,
                CTF=CTF,
                imgAll=img_all,
                msk2=msk2,
                PD=avg_orientation_vec,
-               Psis=psis,
-               imgAvg=img_avg,
-               imgAvgFlip=img_avg_flip,
-               imgLabels=img_labels,
-               imgAllIntensity=img_avg_intensity,
-               version=version,
-               avg_only=avg_only,
-               relion_data=relion_data)
+               imgAvg=img_avg)
 
 
 def _construct_input_data(thresholded_indices, quats_full, defocus):
@@ -353,7 +324,6 @@ def op(*argv):
                                   img_file_name=p.img_stack_file,
                                   image_offsets=prds.microscope_origin,
                                   n_particles_tot=len(prds.defocus),
-                                  avg_only=False,
                                   relion_data=p.relion_data)
 
     progress1 = argv[0] if use_gui_progress else NullEmitter()

commit 4e66c18418f1d31a6f3a1356eb396f1a3b471240

distances: fix indexing bug introduced in e09b8a5

ManifoldEM/GetDistancesS2.py
@@ -221,7 +221,7 @@ def get_distance_CTF_local(input_data: LocalInput, filter_params: FilterParams, 
         if indices[i_part] < n_particles_tot / 2:  # first half data set; i.e., before augmentation
             raw_particle_index = indices[i_part]
         else:  # second half data set; i.e., the conjugates
-            raw_particle_index = (indices[i_part] - n_particles_tot) // 2
+            raw_particle_index = int(indices[i_part] - n_particles_tot / 2)
         if not relion_data:  # spider data
             start = n_pix**2 * raw_particle_index * 4
             img = np.memmap(img_file_name, dtype='float32', offset=start, mode='r', shape=(n_pix, n_pix)).T